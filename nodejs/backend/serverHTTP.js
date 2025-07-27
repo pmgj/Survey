10633fec8df4dacfa69da2d711597f6668e97aac
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2025-07-27 08:28:27 -0300

    asdfsd

--- nodejs/backend/serverHTTP.js
+++ nodejs/backend/serverHTTP.js
@@ -1,21 +1,30 @@
-// server.mjs
 import { createServer } from 'node:http';
-const server = createServer((req, res) => {
-    if(req.method === "GET") {
-        let payload = "";
-        req.on("data", chunk => {
-            payload += chunk.toString();       
-        });
-        req.on("end", () => {
-            res.end(payload);
-        });
-    }
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!\n');
+const server = createServer((req, res) => {
+
+    // Informar dados individualmente
+    // res.statusCode = 200;
+    // res.statusMessage = "Tudo certo!";
+    // res.setHeader("Content-Type", "text/plain");
+    
+    // Informar os dados em conjunto
+    res.writeHead(200, "Write Head", { 'Content-Type': 'text/plain' });
+    
+    res.end("Hello World!");
+
+    // if (req.method === "POST") {
+    //     let payload = "";
+    //     req.on("data", chunk => payload += chunk.toString());
+    //     req.on("end", () => {
+    //         res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //         res.end(payload);
+    //     });
+    // } else {
+    //     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //     res.end('Hello World!\n');
+    // }
 });
-// starts a simple http server locally on port 3000
+
 server.listen(3002, '127.0.0.1', () => {
     console.log('Listening on 127.0.0.1:3002');
 });
-// run with `node server.mjs`
